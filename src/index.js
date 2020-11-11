833cf2bcba01639b2b32c505ae3f35d12dfebf1a
Dmytro Shalenyi <shalenyi.dm@gmail.com>
2020-11-11 15:15:25 +0200

    end-1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './styles.scss';
+
+const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
+
+let btnStartRef, btnStopRef, intervalId;
+document
+  .querySelectorAll('button')
+  .forEach(el => (el.dataset.action === 'start' ? (btnStartRef = el) : (btnStopRef = el)));
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+btnStopRef.setAttribute('disabled', 'disabled');
+
+btnStartRef.addEventListener('click', startColorSwitcher);
+btnStopRef.addEventListener('click', stopColorSwitcher);
+
+function startColorSwitcher() {
+  btnStopRef.removeAttribute('disabled');
+  btnStartRef.setAttribute('disabled', 'disabled');
+  intervalId = setInterval(() => {
+    const i = randomIntegerFromInterval(0, colors.length - 1);
+    document.body.style.backgroundColor = colors[i];
+  }, 1000);
+}
+
+function stopColorSwitcher() {
+  btnStartRef.removeAttribute('disabled');
+  btnStopRef.setAttribute('disabled', 'disabled');
+  clearInterval(intervalId);
+}
